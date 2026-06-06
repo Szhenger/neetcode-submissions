1b7003ab3cde9bd5f640523090cfd5e821ec31d2
Shuo <115667535+Szhenger@users.noreply.github.com>
2026-06-06 18:49:09 -0400

    Add: find-target-in-rotated-sorted-array - submission-9

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Binary search on the rotated sorted array
+        left, right = 0, len(nums) - 1
+        while left <= right: 
+            # Get the middle index in the rotated sorted subarray
+            middle = (left + right) // 2
+            # Case 1: middle is the target index
+            if nums[middle] == target:
+                return middle
+            # Case 2: middle is in the LEFT sorted subarray
+            elif nums[left] <= nums[middle]:
+                if nums[left] <= target < nums[middle]:
+                    right = middle - 1
+                else:
+                    left = middle + 1
+            # Case 2: middle is in the RIGHT sorted subarray
+            else:
+                if nums[middle] < target <= nums[right]:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+        return -1
