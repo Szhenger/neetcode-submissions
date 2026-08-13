9bb45a1f9398978c0782f32d22fabd0d3b2d1d51
Shuo <115667535+Szhenger@users.noreply.github.com>
2026-08-13 16:18:03 -0400

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Get an empty Python hashmap of num -> idx
+        maps = {} 
+        # Enumerate over the input Python list of integers
+        for idx, num in enumerate(nums):
+            # Get the complement of num
+            com = target - num
+            # Search for the complement
+            if com in maps:
+                return [maps[com], idx]
+            # Insert (num, idx) into the Python hashmap
+            maps[num] = idx
+        
